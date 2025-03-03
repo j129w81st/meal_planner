c5a4b9f4b5bd6a54efe9a5138ce1e4f28d14e22e
Hilly Mandelbaum <hillymandelbaum@Hillys-Air.lan>
2025-03-02 19:03:13 -0500

    Updated app.py to serve menu

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
+# Route for home page (serves index.html)
 @app.route('/')
 def home():
-    return "Meal Planner is Live!"
+    return send_from_directory("my_site", "index.html")
+
+# Route to serve all static files (HTML, CSS, JS)
+@app.route('/<path:filename>')
+def static_files(filename):
+    return send_from_directory("my_site", filename)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
